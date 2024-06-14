7719232483a01a16a67a4efbba1231fd73914c8b
MuhFaridanSutariya <faridansutariya@students.universitasmulia.ac.id>
2024-06-15 01:12:53 +0700

    refactor: switch to uvicorn for running the flask app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from src.api.routes import api_blueprint
+import uvicorn
+
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
@@ -9,4 +11,4 @@ def before_request():
     print(f"Received request: {request.method} {request.url}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=5000, log_level="info") # Change with your host
