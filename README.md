c64bb47467255395f15d20cac75d367e4de942be
MuhFaridanSutariya <faridansutariya@students.universitasmulia.ac.id>
2024-06-20 14:47:28 +0700

    feat: add cors middleware

--- README.md
+++ README.md
@@ -58,4 +58,4 @@ To make a prediction, you can send a `POST` request to the `/predict` endpoint w
 - [Framework PyTorch](https://lightning.ai/docs/pytorch/stable/common/lightning_module.html)
 
 ### Todo:
-]- CI/CD using Github Action
+- CI/CD using Github Action
--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request
+from flask_cors import CORS
 from src.api.routes import api_blueprint
 import uvicorn
 
-
 app = Flask(__name__)
+
+# Specify the origins allowed
+origins = [
+    "http://localhost:8080",
+    "localhost:8080",
+]
+
+CORS(app, origins=origins, supports_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+
 app.register_blueprint(api_blueprint)
 
 @app.before_request
